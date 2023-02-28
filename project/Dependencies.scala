62162a478650e319426b1c787068282de4dc1094
Nicolas Vollmar <nvollmar@gmail.com>
2023-02-28 11:00:03 +0100

    Updates dependencies (#19)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,16 +7,16 @@ object Dependencies {
   val Scala213 = "2.13.10"
   val ScalaVersions = Seq(Scala212, Scala213)
 
-  val AkkaVersion = System.getProperty("override.akka.version", "2.6.9")
+  val AkkaVersion = System.getProperty("override.akka.version", "2.6.20")
   val AkkaVersionInDocs = AkkaVersion.take(3)
   val CassandraVersionInDocs = "4.0"
   // Should be sync with the version of the driver in Alpakka Cassandra
   val DriverVersionInDocs = "4.6"
 
-  val AlpakkaVersion = "2.0.2"
+  val AlpakkaVersion = "3.0.4"
   val AlpakkaVersionInDocs = AlpakkaVersion.take(3)
   // for example
-  val AkkaManagementVersion = "1.0.6"
+  val AkkaManagementVersion = "1.1.4"
 
   val Logback = "ch.qos.logback" % "logback-classic" % "1.2.10"
 
@@ -40,9 +40,9 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-persistence" % AkkaVersion,
     "com.typesafe.akka" %% "akka-persistence-query" % AkkaVersion,
     "com.typesafe.akka" %% "akka-cluster-tools" % AkkaVersion,
-    "org.scala-lang.modules" %% "scala-collection-compat" % "2.4.4",
+    "org.scala-lang.modules" %% "scala-collection-compat" % "2.7.0",
     Logback % Test,
-    "org.scalatest" %% "scalatest" % "3.2.11" % Test,
+    "org.scalatest" %% "scalatest" % "3.2.14" % Test,
     "org.pegdown" % "pegdown" % "1.6.0" % Test,
     "org.osgi" % "org.osgi.core" % "5.0.0" % Provided) ++ akkaTestDeps.map(_ % AkkaVersion % Test)
 
