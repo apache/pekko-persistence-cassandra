c216a1f7a612353dd31880356f61c6dfad3703d3
PJ Fanning <pjfanning@users.noreply.github.com>
2023-08-19 18:13:44 +0100

    fix scala3 build issue (#86)
    * fix scala3 build issue
    
    * Update Common.scala

--- project/Common.scala
+++ project/Common.scala
@@ -57,10 +57,13 @@ object Common extends AutoPlugin {
         s"https://github.com/apache/incubator-pekko-persistence-cassandra/tree/${branch}€{FILE_PATH_EXT}#L€{FILE_LINE}"
       },
       "-doc-canonical-base-url",
-      "https://pekko.apache.org/api/pekko-persistence-cassandra/current/",
-      "-skip-packages",
-      "org.apache.pekko.pattern" // for some reason Scaladoc creates this
-    ),
+      "https://pekko.apache.org/api/pekko-persistence-cassandra/current/") ++ {
+      // for some reason Scaladoc creates this
+      if (scalaBinaryVersion == "3")
+        List("-skip-packages:org.apache.pekko.pattern")
+      else
+        List("-skip-packages", "org.apache.pekko.pattern")
+    },
     Compile / doc / scalacOptions --= Seq("-Xfatal-warnings"),
     autoAPIMappings := true,
     Test / logBuffered := System.getProperty("pekko.logBufferedTests", "false").toBoolean,
