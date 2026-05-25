803f438d256ea8eb8ab408c9660e0deed3c48214
PJ Fanning <pjfanning@users.noreply.github.com>
2026-05-25 13:38:43 +0100

    Update EventProcessorStream.scala

--- example/src/main/scala/org/apache/pekko/persistence/cassandra/example/EventProcessorStream.scala
+++ example/src/main/scala/org/apache/pekko/persistence/cassandra/example/EventProcessorStream.scala
@@ -12,15 +12,15 @@ package org.apache.pekko.persistence.cassandra.example
 import org.apache.pekko
 import pekko.{ Done, NotUsed }
 import pekko.actor.typed.ActorSystem
+import pekko.actor.typed.scaladsl.LoggerOps
 import pekko.persistence.cassandra.query.scaladsl.CassandraReadJournal
 import pekko.persistence.query.{ Offset, PersistenceQuery, TimeBasedUUID }
 import pekko.persistence.typed.PersistenceId
-import pekko.stream.SharedKillSwitch
+import pekko.stream.{ RestartSettings, SharedKillSwitch }
 import pekko.stream.connectors.cassandra.scaladsl.CassandraSessionRegistry
 import pekko.stream.scaladsl.{ RestartSource, Sink, Source }
 import com.datastax.oss.driver.api.core.cql.{ PreparedStatement, Row }
 import org.slf4j.{ Logger, LoggerFactory }
-import pekko.actor.typed.scaladsl.LoggerOps
 import org.HdrHistogram.Histogram
 
 import scala.concurrent.{ ExecutionContext, Future }
